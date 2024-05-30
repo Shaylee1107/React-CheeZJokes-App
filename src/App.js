7e3c42ff5f0a3080e3d64ca75929a3915edf13e3
Owner <shayleebaumgartner2002@gmail.com>
2024-05-30 18:41:22 -0400

    refacturing the JokeList component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import JokeList from "./JokeList";
 
 const App = () => {
--- src/Joke.js
+++ src/Joke.js
@@ -1,20 +1,41 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import "./Joke.css";
 
-/** A single joke, along with vote up/down buttons. */
+// class Joke extends Component {
+//   render() {
+//     const { id, vote, votes, text } = this.props;
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
+//     return (
+//       <div className="Joke">
+//         <div className="Joke-votearea">
+//           <button onClick={evt => vote(id, +1)}>
+//             <i className="fas fa-thumbs-up" />
+//           </button>
+
+//           <button onClick={evt => vote(id, -1)}>
+//             <i className="fas fa-thumbs-down" />
+//           </button>
+
+//           {votes}
+//         </div>
+
+//         <div className="Joke-text">{text}</div>
+//       </div>
+//     );
+//   }
+// }
+
+const Joke = ({id, votes, text}) => {
+  const [vote, setVote] = useState(0);
 
     return (
       <div className="Joke">
         <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
+          <button onClick={() => vote(id, +1)}>
             <i className="fas fa-thumbs-up" />
           </button>
 
-          <button onClick={evt => vote(id, -1)}>
+          <button onClick={() => vote(id, -1)}>
             <i className="fas fa-thumbs-down" />
           </button>
 
@@ -24,7 +45,7 @@ class Joke extends Component {
         <div className="Joke-text">{text}</div>
       </div>
     );
-  }
+  
 }
 
 export default Joke;

dbfc5d7206593db7423f0448fedb2b28683a4f3b
Owner <shayleebaumgartner2002@gmail.com>
2024-05-30 19:18:22 -0400

    removing commented out original code and cleaning up

--- src/Joke.js
+++ src/Joke.js
@@ -1,30 +1,6 @@
 import React, {useState} from "react";
 import "./Joke.css";
 
-// class Joke extends Component {
-//   render() {
-//     const { id, vote, votes, text } = this.props;
-
-//     return (
-//       <div className="Joke">
-//         <div className="Joke-votearea">
-//           <button onClick={evt => vote(id, +1)}>
-//             <i className="fas fa-thumbs-up" />
-//           </button>
-
-//           <button onClick={evt => vote(id, -1)}>
-//             <i className="fas fa-thumbs-down" />
-//           </button>
-
-//           {votes}
-//         </div>
-
-//         <div className="Joke-text">{text}</div>
-//       </div>
-//     );
-//   }
-// }
-
 const Joke = ({text}) => {
   const [votes, setVotes] = useState(0);
 
